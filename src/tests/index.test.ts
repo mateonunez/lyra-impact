4326b9d08e16e210da003d6de7921f321949d4bf
mateonunez <mateonunez95@gmail.com>
2022-08-21 16:20:30 +0200

    test(impact): add basic test

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,6 +1,17 @@
-import t from 'tap'
+import { search } from "@nearform/lyra";
+import t from "tap";
+import { impact } from "..";
 
-t.test('test', t => {
-  t.pass('pass')
-  t.end()
-})
+t.test("should retrieve the data and create a Lyra instance", (t) => {
+  t.plan(1);
+
+  impact(
+    "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
+  ).then((lyra) => {
+    const result = search(lyra, {
+      term: "pikachu",
+    });
+
+    t.equal(result.count, 1);
+  });
+});
