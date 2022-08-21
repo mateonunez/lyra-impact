25ab3c63b61df5f24a6b77188bd7dd834bbdddd9
mateonunez <mateonunez95@gmail.com>
2022-08-21 16:01:59 +0200

    feat(fetcher): add options

--- src/common/fetcher.ts
+++ src/common/fetcher.ts
@@ -1,8 +1,30 @@
-export default async function fetcher (url: string): Promise<any> {
+export type FetchOptions = {
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  headers?: {[key: string]: string};
+  selector?: (...args: any[]) => any;
+  property?: string;
+}
+
+export default async function fetcher (url: string, options: FetchOptions): Promise<[]> {
+  const { method = "GET", headers = {}, selector, property = null } = options;
+
   const response = await fetch(url, {
+    method,
     headers: {
       Accept: "application/json",
+      ...headers,
     }
   })
-  return await response.json();
+
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+
+  let data = await response.json()
+
+  if (property) {
+    data = data[property]
+  }
+
+  return data;
 }
