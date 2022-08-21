c675d34a94c59c2933be0d3d85576424c42a554e
mateonunez <mateonunez95@gmail.com>
2022-08-21 17:10:46 +0200

    test(*): add more cases

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -2,16 +2,82 @@ import { search } from "@nearform/lyra";
 import t from "tap";
 import { impact } from "..";
 
-t.test("should retrieve the data and create a Lyra instance", (t) => {
+t.test("should retrieve the data and create a Lyra instance", t => {
+  t.plan(2);
+
+  t.test("should retrieve the data and create a Lyra instance", t => {
+    t.plan(1);
+    impact("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json").then(lyra => {
+      const result = search(lyra, {
+        term: "pikachu",
+      });
+
+      console.log({hits: result.count});
+
+      t.equal(result.count, 1);
+    });
+  });
+
+  t.test("should retrieve the data using a custom property", t => {
+    t.plan(1);
+    // the data is in the "results" property {info: {...}, results: [{...}]}
+    impact("https://rickandmortyapi.com/api/character/", {
+      property: "results",
+    }).then(lyra => {
+      const result = search(lyra, {
+        term: "rick",
+      });
+
+      t.equal(result.count, 4);
+    });
+  })
+});
+
+t.test("should resolve the schema", t => {
+  t.plan(1)
+
+  t.test("should resolve with impact data", t => {
+    t.plan(1)
+
+    impact("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json").then(lyra => {
+      const expectedSchema = {
+        id: "number",
+        name: {
+          english: "string",
+          japanese: "string",
+          chinese: "string",
+          french: "string",
+        },
+        type: {
+          "0": "string",
+          "1": "string",
+          "2": "string",
+          "3": "string",
+          "4": "string",
+        },
+        base: {
+          HP: "number",
+          Attack: "number",
+          Defense: "number",
+          "Sp. Attack": "number",
+          "Sp. Defense": "number",
+          Speed: "number",
+        },
+      }
+  
+      t.match(lyra.schema, expectedSchema);
+    })
+  })
+});
+
+t.test("errors", t => {
   t.plan(1);
 
-  impact(
-    "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
-  ).then((lyra) => {
-    const result = search(lyra, {
-      term: "pikachu",
-    });
+  t.test("should throw an error when the data is not a valid JSON", t => {
+    t.plan(1);
 
-    t.equal(result.count, 1);
+    impact("https://raw.githubusercontent.com/falsy/pokemon.json/falsy/pokedex.json", ).catch(err => {
+      t.equal(err.message, "The request failed: 404");
+    });
   });
 });
