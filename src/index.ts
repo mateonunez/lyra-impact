9df4d7b851b992685c67f549859788b096202e92
mateonunez <mateonunez95@gmail.com>
2022-08-22 11:22:47 +0200

    feat(impact): add type and options

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
-import {Lyra, PropertiesSchema, create, insert} from "@nearform/lyra"
-import fetcher, {FetchOptions} from "./common/fetcher"
+import {create, insert} from "@nearform/lyra"
+import fetcher from "./common/fetcher"
 import {resolveSchema} from "./common/schema"
+import type {Lyra, PropertiesSchema, Configuration as LyraConfiguration} from "@nearform/lyra"
+import type {FetchOptions} from "./common/fetcher"
 
-export async function impact<T extends PropertiesSchema>(url: string, options?: FetchOptions): Promise<Lyra<T>> {
-  const data = await fetcher(url, {
+export type ImpactOptions = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  lyra?: LyraConfiguration<any>
+  fetch?: FetchOptions
+  property?: string
+}
+
+export async function impact<T extends PropertiesSchema>(url: string, options?: ImpactOptions): Promise<Lyra<T>> {
+  const fetcherOptions = {
     method: "GET",
-    ...options
-  })
+    property: options?.property,
+    ...options?.fetch
+  }
+  const data = await fetcher(url, {...fetcherOptions})
 
   const schema = resolveSchema({}, data)
-
-  const lyra = create({
-    schema,
-    defaultLanguage: "english"
-  })
+  const lyraOptions = {...options?.lyra}
+  const lyra = create({schema, ...lyraOptions})
 
   for (const entry of data) {
     insert(lyra, entry)
