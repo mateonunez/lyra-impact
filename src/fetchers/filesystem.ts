27639879fd9bf08756bf883676d185f9847c1820
mateonunez <mateonunez95@gmail.com>
2022-09-07 09:12:07 +0200

    feat(filesystem): add basic fetcher

--- src/fetchers/filesystem.ts
+++ src/fetchers/filesystem.ts
@@ -1,8 +1,22 @@
-import {FilesystemOptions} from "."
-import {isServer} from "../utils"
+import {FetcherOptions, FilesystemOptions} from "."
+import {isServer, parseData} from "../utils"
+import fs from "fs"
+import path from "path"
 
-export default async function filesystemFetcher(path: string, options: FilesystemOptions): Promise<[]> {
+export default async function filesystemFetcher(url: string, options: FetcherOptions<FilesystemOptions>): Promise<[]> {
   if (!isServer) throw new Error("Filesystem fetcher is not supported in browser")
 
-  return []
+  // const {property} = options
+
+  // check if file exists
+  if (!fs.existsSync(url)) throw new Error(`File not found: ${path}`)
+
+  const extension = path.extname(url)
+  const dataFile = fs.readFileSync(url, "utf8")
+
+  const data = parseData(dataFile, {
+    extension
+  })
+
+  return data
 }
