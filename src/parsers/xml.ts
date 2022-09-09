5557b937d33b664439be934b39848d3f7e420bc5
mateonunez <mateonunez95@gmail.com>
2022-09-09 11:29:09 +0200

    feat(*): add xml2js dependency and implement parser

--- src/parsers/xml.ts
+++ src/parsers/xml.ts
@@ -1,15 +1,44 @@
+import {parseString} from "xml2js"
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function parseXml(data: string, property?: string): any {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const dataParsed: any = {}
+  let dataParsed: any = {}
 
-  const xmlParsed = data.matchAll(/(?:<(\w*)(?:\s[^>]*)*>)((?:(?!<\1).)*)(?:<\/\1>)|<(\w*)(?:\s*)*\/>/gm)
+  parseString(
+    data,
+    {
+      trim: true,
+      explicitArray: false
+    },
+    (err: any, result) => {
+      if (err) {
+        throw new Error(err)
+      }
 
-  for (const match of xmlParsed) {
-    console.log({match})
-    const key = match[1] || match[3]
-    const value = match[2] && parseXml(match[2], property)
-    dataParsed[key] = (value && Object.keys(value).length ? value : match[2]) || null
+      dataParsed = result
+    }
+  )
+
+  if (property && dataParsed) {
+    const isNestedProperty = property.includes(".")
+    const propertyArray = property.split(".")
+
+    if (isNestedProperty) {
+      let i = 0
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      let dataParsedNested: any = dataParsed
+
+      while (i < propertyArray.length) {
+        dataParsedNested = dataParsedNested[propertyArray[i]]
+        i++
+      }
+
+      dataParsed = dataParsedNested
+    } else {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      dataParsed = (dataParsed as any)[property]
+    }
   }
 
   return dataParsed
