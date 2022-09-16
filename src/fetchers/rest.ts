c9349c671fa2269d1a84ba5b4fb34e579d8b6d92
mateonunez <mateonunez95@gmail.com>
2022-09-16 15:41:02 +0200

    chore(rest): lint and format

--- src/fetchers/rest.ts
+++ src/fetchers/rest.ts
@@ -1,4 +1,3 @@
-// import p from "phin"
 import {parseData} from "../utils"
 import type {FetcherOptions, RestOptions} from "."
 
@@ -7,12 +6,6 @@ export default async function restFetcher(url: string, options: FetcherOptions<R
 
   if (options.property) delete options.property
 
-  // const response = await p({
-  //   url,
-  //   method,
-  //   ...options
-  // })
-
   const response = await fetch(url, {
     method,
     ...options
@@ -20,16 +13,14 @@ export default async function restFetcher(url: string, options: FetcherOptions<R
 
   if (!response) throw new Error("No response")
 
-  const contentType = response.headers.get("content-type") || "application/json"
+  const contentType = response?.headers.get("content-type") || "application/json"
   const extension = url?.split(".").pop() || "json"
 
   if (!response.status || response?.status > 299 || response?.status < 200) {
     throw new Error(`Error fetching data from ${url}: ${response.status} ${response.statusText}`)
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const text = await response.text()
-  console.log({text})
   const data = parseData(text, {
     contentType,
     extension,
