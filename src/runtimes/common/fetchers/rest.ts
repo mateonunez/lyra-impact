3d7103b8dbade1301427f113a05b259eb2ed9a65
mateonunez <mateonunez95@gmail.com>
2022-09-20 12:18:26 +0200

    perf(fetchers): clean up and remove useless modules

--- src/runtimes/common/fetchers/rest.ts
+++ src/runtimes/common/fetchers/rest.ts
@@ -1,6 +1,6 @@
 import {parseData} from "../../../utils"
 import type {FetcherOptions, RestOptions} from "../../../types"
-import {NO_RESPONSE_FROM_SERVER, RESPONSE_INVALID} from "../../../errors"
+import {RESPONSE_INVALID} from "../../../errors"
 
 export default async function restFetcher(url: string, options: FetcherOptions<RestOptions>): Promise<[]> {
   const {method = "GET", property} = options
@@ -12,14 +12,12 @@ export default async function restFetcher(url: string, options: FetcherOptions<R
     ...options
   })
 
-  if (!response) throw new Error(NO_RESPONSE_FROM_SERVER())
-
-  const contentType = response?.headers.get("content-type") || "application/json"
-  const extension = url?.split(".").pop() || "json"
-
   if (!response.status || response?.status > 299 || response?.status < 200) {
     throw new Error(RESPONSE_INVALID(url, response.status, response.statusText))
   }
+
+  const contentType = response?.headers.get("content-type") || "application/json"
+  const extension = url?.split(".").pop() || "json"
 
   const text = await response.text()
   const data = parseData(text, {
