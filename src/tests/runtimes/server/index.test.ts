5d6ced5d1f2f114e87712a579cd70925b9fd8144
mateonunez <mateonunez95@gmail.com>
2022-09-27 13:37:43 +0200

    test(server): refactor tests and remove old tests

--- src/tests/runtimes/server/index.test.ts
+++ src/tests/runtimes/server/index.test.ts
@@ -1,11 +1,179 @@
+import {search} from "@lyrasearch/lyra"
 import t from "tap"
+import {collision, impact} from "../../../runtimes/server"
 import {getContentType} from "../../../runtimes/server/fetchers/filesystem"
 import {getExtensionFromUrl} from "../../../utils"
 
-t.test("server runtime", t => {
-  t.plan(2)
+const pokedex = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
+const countries = "https://cdn.wsform.com/wp-content/uploads/2018/09/country_full.csv"
+const rickMorty = "https://rickandmortyapi.com/api/character"
+const rickMortyGraphql = "https://rickandmortyapi.com/graphql"
 
-  t.test("the content type should be computed correctly using the extension", t => {
+t.test("server runtime", t => {
+  t.plan(4)
+
+  t.test("impact", async t => {
+    t.plan(3)
+
+    t.test("rest fetcher", async t => {
+      t.test("should create a lyra instance", async t => {
+        t.plan(3)
+
+        t.test("without property", async t => {
+          const lyra = await impact(pokedex)
+          const result = search(lyra, {term: "pikachu"})
+
+          t.equal(result.count, 1)
+          t.end()
+        })
+
+        t.test("with property", async t => {
+          const lyra = await impact(rickMorty, {property: "results"})
+          const result = search(lyra, {term: "rick", properties: ["name"]})
+
+          t.equal(result.count, 4)
+          t.end()
+        })
+
+        t.test("from online csv", async t => {
+          const lyra = await impact(countries)
+          const result = search(lyra, {term: "Colombia"})
+
+          t.equal(result.count, 1)
+          t.end()
+        })
+      })
+      t.end()
+    })
+
+    t.test("graphql fetcher", async t => {
+      t.test("should create a lyra instance", async t => {
+        t.plan(1)
+
+        t.test("with property", async t => {
+          const lyra = await impact(rickMortyGraphql, {
+            fetch: {
+              fetcher: "graphql",
+              query: `{
+                characters {
+                  results {
+                    type
+                    status
+                    species
+                    name
+                    id
+                  }
+                }
+              }`,
+              property: "characters.results"
+            }
+          })
+          const result = search(lyra, {term: "rick", properties: ["name"]})
+
+          t.equal(result.count, 4)
+          t.end()
+        })
+      })
+      t.end()
+    })
+
+    t.test("filesystem fetcher", async t => {
+      t.test("should create a lyra instance", async t => {
+        t.plan(1)
+
+        t.test("json", async t => {
+          const lyra = await impact("./package.json", {fetch: {fetcher: "filesystem"}})
+          const result = search(lyra, {term: "mateonunez"})
+
+          t.equal(result.count, 1)
+          t.end()
+        })
+      })
+      t.end()
+    })
+  })
+
+  t.test("collision", async t => {
+    t.plan(3)
+
+    t.test("rest fetcher", async t => {
+      t.test("should create a lyra instance", async t => {
+        t.plan(3)
+
+        t.test("without property", async t => {
+          const {hits} = await collision(pokedex, {term: "pikachu"})
+
+          t.equal(hits.length, 1)
+          t.end()
+        })
+
+        t.test("with property", async t => {
+          const {hits} = await collision(rickMorty, {term: "rick", properties: ["name"]}, {property: "results"})
+
+          t.equal(hits.length, 4)
+          t.end()
+        })
+
+        t.test("from online csv", async t => {
+          const {hits} = await collision(countries, {term: "Colombia"})
+
+          t.equal(hits.length, 1)
+          t.end()
+        })
+      })
+      t.end()
+    })
+
+    t.test("graphql fetcher", async t => {
+      t.test("should create a lyra instance", async t => {
+        t.plan(1)
+
+        t.test("with property", async t => {
+          const {hits} = await collision(
+            rickMortyGraphql,
+            {term: "rick", properties: ["name"]},
+            {
+              fetch: {
+                fetcher: "graphql",
+                query: `{
+                characters {
+                  results {
+                    type
+                    status
+                    species
+                    name
+                    id
+                  }
+                }
+              }`,
+                property: "characters.results"
+              }
+            }
+          )
+
+          t.equal(hits.length, 4)
+          t.end()
+        })
+      })
+      t.end()
+    })
+
+    t.test("filesystem fetcher", async t => {
+      t.test("should create a lyra instance", async t => {
+        t.plan(1)
+
+        t.test("json", async t => {
+          const {hits} = await collision("./package.json", {term: "mateonunez"}, {fetch: {fetcher: "filesystem"}})
+
+          t.equal(hits.length, 1)
+          t.end()
+        })
+      })
+      t.end()
+    })
+  })
+
+  t.test("content-type", t => {
     const json = getContentType("json")
     const csv = getContentType("csv")
     const txt = getContentType("txt")
@@ -16,7 +184,7 @@ t.test("server runtime", t => {
     t.end()
   })
 
-  t.test("the extension should be computed correctly using the url", t => {
+  t.test("extensions", t => {
     const json = getExtensionFromUrl("https://example.com/pokemon.json")
     const csv = getExtensionFromUrl("https://example.com/pokemon.csv")
     const txt = getExtensionFromUrl("https://example.com/pokemon.txt")
